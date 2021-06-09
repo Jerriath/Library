6dbbfc4b78180e7aec41d334d0adaa70bebfec56
Jerry Zhang <jerryzhang101@yahoo.com>
2021-06-09 16:45:28 -0400

    Added all necessary ui elements and a couple of functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+//Important variables
+const bookShelf = document.querySelector("#library");
+const addBookBtn = document.querySelector("#addBook");
+const addBookDiv = document.querySelector("#addBookDiv")
+const stats = document.querySelector("#stats");
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,17 +19,49 @@ function Book(title, author, pages, read) {
 
 let library = {
     shelf: new Array(),
-    addBookToLibrary: function(book) {
-        this.shelf.push(book);
+    //Function to create a new div element to add to DOM
+    addToDOM: function(book) {
+        let newBook = document.createElement("div");
+        newBook.classList.add("book");
+        let bookText = document.createElement("h2");
+        bookText.textContent = book.title;
+        bookText.classList.add("bookText");
+        newBook.appendChild(bookText);
+        let info = document.createElement("button");
+        info.textContent = "info";
+        info.addEventListener("click", function(e){
+            showInfo(e);
+        });
+        info.classList.add("infoBtn");
+        newBook.appendChild(info);
+        bookShelf.insertBefore(newBook, addBookDiv);
     },
+    //Function to add a new book to the shelf array
+    addToLibrary: function(book) {
+        this.shelf.push(book);
+        this.addToDOM(book);
+    },
+    //Function for displaying current books in console
     displayBooks: function() {
         let numOfBooks = this.shelf.length;
         for (let i = 0; i < numOfBooks; i++) 
         {
             console.log(this.shelf[i].info());
         }
-    }
+    },
 };
 
+//Function for info button functionality (NOT IMPLEMENTED)
+function showInfo(e) {
+    return;
+}
+
+addBookBtn.addEventListener("click", function(e) {
+    console.log(e);
+});
+
+
+
+//Test books
 let hp = new Book("Harry Potter and the Halfblood Prince", "J. K. Rowling", "hella", "not read yet");
 let hobbit = new Book("The Hobbit", "J. R. R. Tolkien", "HELLA", "not read yet");
